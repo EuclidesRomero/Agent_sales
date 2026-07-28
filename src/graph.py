b783da7c3ecf6a095797803a0fb19b3fb3d878b2
Euclides Romero <98054023+EuclidesRomero@users.noreply.github.com>
2026-07-27 21:54:57 -0500

    Add async PostgreSQL checkpoint and optimize routing logic

--- src/graph.py
+++ src/graph.py
@@ -1,4 +1,8 @@
+import os
+
 from langgraph.graph import StateGraph, END
+from langgraph.checkpoint.postgres.aio import AsyncPostgresSaver
+
 from src.state.state import AgentState
 from src.nodes.intent import handled_intent
 from src.nodes.bussines_information import business_information_node
@@ -6,33 +10,32 @@ from src.nodes.product_and_service_knowledge import product_service_knowledge_no
 from src.nodes.handled_transaction import transaction_node
 from src.nodes.human_assistance import human_assistance_node
 from src.nodes.out_of_scope import out_of_scope_node
-from src.router.router_by_intent import route_by_intent
+from src.router.router_by_intent import route_by_intent, route_entry
+
+CHECKPOINTER_DATABASE_URL = os.getenv("CHECKPOINTER_DATABASE_URL")
 
 
-def build_sales_agent_graph():
+async def build_sales_agent_graph():
     workflow = StateGraph(AgentState)
-    
+
+    checkpointer_cm = AsyncPostgresSaver.from_conn_string(CHECKPOINTER_DATABASE_URL)
+    checkpointer = await checkpointer_cm.__aenter__()
+    await checkpointer.setup()
+
     workflow.add_node("classify_intent", handled_intent)
     workflow.add_node("handle_business_info", business_information_node)
     workflow.add_node("handle_product_query", product_service_knowledge_node)
     workflow.add_node("handle_transaction", transaction_node)
     workflow.add_node("handle_human_request", human_assistance_node)
     workflow.add_node("handle_out_of_scope", out_of_scope_node)
-    
-    workflow.set_entry_point("classify_intent")
-    
-    workflow.add_conditional_edges(
-        "classify_intent",
-        route_by_intent
-    )
-    
+
+    workflow.set_conditional_entry_point(route_entry)
+    workflow.add_conditional_edges("classify_intent", route_by_intent)
+
     workflow.add_edge("handle_business_info", END)
     workflow.add_edge("handle_product_query", END)
     workflow.add_edge("handle_transaction", END)
     workflow.add_edge("handle_human_request", END)
     workflow.add_edge("handle_out_of_scope", END)
-    
-    return workflow.compile()
 
-
-sales_agent = build_sales_agent_graph()
+    return workflow.compile(checkpointer=checkpointer)
